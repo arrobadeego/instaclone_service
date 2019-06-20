3511b5119aba29ab5fd8b9e38c3d0e4a6a436d10
Diego Moreira <diegomptcc@hotmail.com>
2019-06-20 11:14:37 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://admin:12345@cluster0-vybfh.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
+
+app.use(cors());
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 

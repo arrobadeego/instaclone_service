a4379afd04cae11ac88402c3867583b86c785904
Diego Moreira <diegomptcc@hotmail.com>
2019-06-20 11:12:03 -0300

    change extension to jpg before save image

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://admin:12345@cluster0-vybfh.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes.js')); 
 

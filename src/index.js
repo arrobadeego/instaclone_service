f58a9d4f34eceae962d06a134e552792cfe56279
Diego Moreira <diegomptcc@hotmail.com>
2019-06-19 21:30:23 -0300

    configure nodemon

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.send('Hello world');
+    return res.send('hello world');
 });
 
 app.listen(3333);
